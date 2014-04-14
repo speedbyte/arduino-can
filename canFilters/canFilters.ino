635c56e021f949573982723f6d3711ec53c44a83
juliorod <rodriguezm.juliocesar@gmail.com>
2014-04-14 16:29:50 +0200

    First filter test, working with all filters with the same confit

---
+++ canFilters/canFilters.ino
@@ -0,0 +1,82 @@
+// demo: CAN-BUS Shield, receive data
+#include <mcp_can.h>
+#include <SPI.h>
+
+long unsigned int rxId;
+unsigned char len = 0;
+unsigned char rxBuf[8];
+
+//MCP_CAN CAN0(10);                               // Set CS to pin 10
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  CAN.begin(CAN_100KBPS);                       // init can bus : baudrate = 500k 
+  pinMode(2, INPUT);                            // Setting pin 2 for /INT input
+
+  //Configure the CAN controller
+  //configure mask and filters for the messages
+  //we receive only std id with id 123 or 111
+  CAN.init_Mask(0, 0, 0x0123);
+  CAN.init_Filt(0, 0, 0x0123);
+  CAN.init_Filt(1, 0, 0x0123);
+  
+  //Buffer 1
+  CAN.init_Mask(1, 0, 0x0123);
+  CAN.init_Filt(2, 0, 0x0123);
+  CAN.init_Filt(3, 0, 0x0123);
+  CAN.init_Filt(4, 0, 0x0123);
+  CAN.init_Filt(5, 0, 0x0123);
+
+
+  Serial.println("MCP2515 Library Receive Example...");
+  
+   
+}
+
+void loop()
+{
+  /*  
+  if(!digitalRead(2))                         // If pin 2 is low, read receive buffer
+    {
+      CAN0.readMsgBuf(&len, rxBuf);              // Read data: len = data length, buf = data byte(s)
+      rxId = CAN0.getCanId();                    // Get message ID
+      Serial.print("ID: ");
+      Serial.print(rxId, HEX);
+      Serial.print("  Data: ");
+      for(int i = 0; i<len; i++)                // Print each byte of the data
+      {
+        if(rxBuf[i] < 0x10)                     // If data byte is less than 0x10, add a leading zero
+        {
+          Serial.print("0");
+        }
+        Serial.print(rxBuf[i], HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+    }
+    */
+    if(CAN.checkReceive()==3){
+     CAN.readMsgBuf(&len, rxBuf);              // Read data: len = data length, buf = data byte(s)
+      rxId = CAN.getCanId();                    // Get message ID
+      Serial.print("ID: ");
+      Serial.print(rxId, HEX);
+      Serial.print("  Data: ");
+      for(int i = 0; i<len; i++)                // Print each byte of the data
+      {
+        if(rxBuf[i] < 0x10)                     // If data byte is less than 0x10, add a leading zero
+        {
+          Serial.print("0");
+        }
+        Serial.print(rxBuf[i], HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+    }
+}
+
+/*********************************************************************************************************
+  END FILE
+*********************************************************************************************************/
